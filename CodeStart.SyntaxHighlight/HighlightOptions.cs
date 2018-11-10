8f81828d46903a7f07ba4bf8305192ee56c1fe67
aangairbender <jetixranger@ya.ru>
2018-11-10 18:22:37 +0200

    Highlight stuff add

---
+++ CodeStart.SyntaxHighlight/HighlightOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace CodeStart.SyntaxHighlight
+{
+    /// <summary>
+    /// Highlight style enum
+    /// </summary>
+    [Flags]
+    public enum HighlightStyle
+    {
+        Default = 0,
+        Bold = 1 << 0,
+        Italics = 1 << 1,
+        Underlined = 1 << 2
+    }
+
+    /// <summary>
+    /// Highlight options
+    /// </summary>
+    public struct HighlightOptions
+    {
+        /// <summary> Foreground color </summary>
+        public Color ForegroundColor { get; set; }
+
+        /// <summary> Background color </summary>
+        public Color BackgroundColor { get; set; }
+
+        /// <summary> Style </summary>
+        public HighlightStyle Style { get; set; }
+    }
+}
---
+++ CodeStart.SyntaxHighlight/ISyntaxHighlighter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CodeStart.SyntaxHighlight
+{
+    /// <summary> Syntax highlighter interface </summary>
+    interface ISyntaxHighlighter
+    {
+        /// <summary>
+        /// Highlight specified text
+        /// </summary>
+        /// <param name="text">Text to highlight</param>
+        /// <returns>
+        /// Dictionary with highlight options.
+        /// Key is position of the character.
+        /// Value is <see cref="HighlightOptions"/> for the character.
+        /// </returns>
+        IDictionary<int, HighlightOptions> Highlight(string text);
+    }
+}
